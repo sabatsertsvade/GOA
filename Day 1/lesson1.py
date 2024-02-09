c983a2a1649548680f68c7bf309b8889962729cf
sabatsertsvade <tsertsvadzesaba2011@gmail.com>
2024-02-09 21:34:48 +0300

    first commit

---
+++ Day 1/lesson1.py
@@ -0,0 +1,32 @@
+name = "saba"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობისთვის მიმნიჭებელი სიმბოლო
+# "saba" არის ცვლადისთვის მნიშვნელობა
+
+surname = "tsertsvadze" 
+
+print(name)
+# პრინტ ფუნქციას გადაეცემ ეკრანზე გამოსატანი ობიექტი
+
+name  = "saba"  # ეს არის str(string) ტიპის ცვლადი
+age = 12 # ეს არის int (integer) მთელი რიცხვი
+height = 160.5 #ეს არის flaat ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming= True #true ან false
+is_ugly = False  #snakecase  ( უბრალოდ წერის სტილისტანდარტულად )
+
+isUgly = False # ჯავასკრიპტული camelcase
+
+#print(name +''+ surname)
+
+
+#სტრინგ არის ბრჭყალეებში მოქცეუული 
+#print(name+age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+print(name + str(age))
