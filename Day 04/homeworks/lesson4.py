e3d5604ed89c6e5d1ffd3e92a6138e0ea5efccd5
sabatsertsvade <tsertsvadzesaba2011@gmail.com>
2024-02-23 21:33:27 +0300

    commit

---
+++ Day 04/homeworks/lesson4.py
@@ -0,0 +1,7 @@
+mather = 33 
+father = 44
+sister = 7
+
+print("my mathers age in 5 years gonna be",mather + 5)
+print("my fathers age in 5 years gonna be",father + 5)
+print("my sisters age in 5 years gonna be",sister + 5)
